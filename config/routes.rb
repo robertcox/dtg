f06b37750e4f6917e8d63088bb3b872052ec894e
robertcox <robert.m.cox@gmail.com>
2013-10-16 16:18:47 -0400

    Added StaticPages controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Dtg::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/help"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
