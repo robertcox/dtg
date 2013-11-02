e10b0b6236bc7969d46ba1df11ac00cce6798b3c
robertcox <robert.m.cox@gmail.com>
2013-11-02 13:09:17 -0400

    Added sign in and sign out functionality.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,17 +3,18 @@ require 'spec_helper'
 describe User do
 
   before  do 
-  	@user = User.new(name: "Example User", email: "user@example.com",
-  					password: "foobar", password_confirmation: "foobar") 
+    @user = User.new(name: "Example User", email: "user@example.com",
+            password: "foobar", password_confirmation: "foobar") 
   end
 
   subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
-  it { should respond_to(:password_digest) }
+  it { should respond_to(:password_digest) }  
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
@@ -88,18 +89,24 @@ describe User do
   end
 
   describe "return value of authenticate method" do
-  before { @user.save }
-  let(:found_user) { User.find_by_email(@user.email) }
+    before { @user.save }
+    let(:found_user) { User.find_by_email(@user.email) }
 
-  	describe "with valid password" do
-    	it { should == found_user.authenticate(@user.password) }
-  	end
+    describe "with valid password" do
+     it { should == found_user.authenticate(@user.password) }
+    end
 
-  	describe "with invalid password" do
-    	let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
 
-    	it { should_not == user_for_invalid_password }
-    	specify { expect(user_for_invalid_password).to be_false }
-  	end
-	end
-end
+      it { should_not == user_for_invalid_password }
+      specify { expect(user_for_invalid_password).to be_false }
+    end
+  end
+
+  describe "remember token" do
+    before { @user.save }
+
+    its(:remember_token) { should_not be_blank }
+  end
+ end
